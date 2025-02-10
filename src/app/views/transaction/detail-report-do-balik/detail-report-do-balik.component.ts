7fc1b4a2950247fd393f264c2cc34de67b86bffc
kingfadjar <kingFadjar1197@gmail.com>
2025-02-10 15:55:18 +0700

    do balik done

--- src/app/views/transaction/detail-report-do-balik/detail-report-do-balik.component.ts
+++ src/app/views/transaction/detail-report-do-balik/detail-report-do-balik.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { DataTableDirective } from 'angular-datatables';
 import { Subject } from 'rxjs';
 import { DataService } from '../../../service/data.service';
@@ -9,169 +15,179 @@ import { ToastrService } from 'ngx-toastr';
 import Swal from 'sweetalert2';
 import * as moment from 'moment';
 import { AppService } from '../../../service/app.service';
-import { DEFAULT_DELAY_TABLE, LS_INV_SELECTED_DELIVERY_ORDER } from '../../../../constants';
+import {
+  DEFAULT_DELAY_TABLE,
+  LS_INV_SELECTED_DELIVERY_ORDER,
+} from '../../../../constants';
+import { TranslationService } from '../../../service/translation.service';
+
+const STATUS_SAME_CONVERSION = 'SAME_CONVERSION';
+
+const ACTION_VIEW = 'view';
 
 @Component({
   selector: 'app-detail-report-do-balik',
   templateUrl: './detail-report-do-balik.component.html',
   styleUrls: ['./detail-report-do-balik.component.scss'],
 })
-export class DetailReportDoBalikComponent implements OnInit, OnDestroy, AfterViewInit {
-  @ViewChild(DataTableDirective, { static: false }) datatableElement: DataTableDirective | undefined;
-  
+export class DetailReportDoBalikComponent
+  implements OnInit, OnDestroy, AfterViewInit
+{
+  columns: any;
+  page: {
+    start: number;
+    length: number;
+    recordsTotal: number;
+    recordsFiltered: number;
+  } = {
+    start: 0,
+    length: 10,
+    recordsTotal: 0,
+    recordsFiltered: 0,
+  };
+
+  orders: any[] = [];
+  dtColumns: any = [];
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
-    selectedOrder: any = JSON.parse(
-      localStorage[LS_INV_SELECTED_DELIVERY_ORDER]
-    );
-  detailDoBalikData: any[] = [];
-  loading: boolean = false;
-  updatingStatus: boolean = false;
-
+  @ViewChild(DataTableDirective, { static: false })
+  datatableElement: DataTableDirective | undefined;
+  selectedOrder: any = JSON.parse(localStorage[LS_INV_SELECTED_DELIVERY_ORDER]);
+  adding: boolean = false;
+  loadingIndicator: boolean = false;
+  showFilterSection: boolean = false;
   disabledCancelButton: boolean = false;
   RejectingOrder: boolean = false;
   totalLength: number = 0;
+  buttonCaptionView: String = 'Lihat';
+  dateRangeFilter: [Date, Date] = [new Date(), new Date()];
+
+  protected config = AppConfig.settings.apiServer;
 
   constructor(
     private dataService: DataService,
     private g: GlobalService,
     private router: Router,
-    private toastr: ToastrService,
-    private appService: AppService
-  ) {}
-
-  ngOnInit(): void {
-    // Ambil data order yang dipilih dari localStorage
+    private appService: AppService,
+    private translation: TranslationService
+  ) {
+    this.g.navbarVisibility = false;
     this.selectedOrder = JSON.parse(this.selectedOrder);
+    this.selectedOrder.TGL_PESANAN = this.g.transformDate(this.selectedOrder.TGL_PESANAN);
+    this.selectedOrder.TGL_TRANSAKSI = this.g.transformDate(this.selectedOrder.TGL_TRANSAKSI);
 
-    // Konfigurasi DataTables
     this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 10,
+      language:
+        this.translation.getCurrentLanguage() == 'id'
+          ? this.translation.idDatatable
+          : {},
       processing: true,
       serverSide: true,
+      autoWidth: true,
+      info: true,
+      drawCallback: () => {},
       ajax: (dataTablesParameters: any, callback) => {
-        this.getDetailDoBalik(callback);
-        // setTimeout(() => {
-        //   this.page.start = dataTablesParameters.start;
-        //   this.page.length = dataTablesParameters.length;
-        // }DEFAULT_DELAY_TABLE)
+        this.page.start = dataTablesParameters.start;
+        this.page.length = dataTablesParameters.length;
+        const params = {
+          noSuratJalan: this.selectedOrder.NO_SURAT_JALAN,
+        };
+        setTimeout(() => {
+          this.dataService
+            .postData(
+              this.config.BASE_URL + '/api/delivery-order/detail-do-balik',
+              params
+            )
+            .subscribe((resp: any) => {
+              const updatedSelectedOrder = {
+                ...this.selectedOrder,
+              };
+              this.selectedOrder = updatedSelectedOrder;
+              this.g.saveLocalstorage(
+                LS_INV_SELECTED_DELIVERY_ORDER,
+                JSON.stringify(updatedSelectedOrder)
+              );
+              const mappedData = resp.data.map((item: any, index: number) => {
+                const { rn, ...rest } = item;
+                const finalData = {
+                  ...rest,
+                  dtIndex: this.page.start + index + 1,
+                };
+                return finalData;
+              });
+              this.page.recordsTotal = resp.recordsTotal;
+              this.page.recordsFiltered = resp.recordsFiltered;
+              this.totalLength = mappedData.length;
+              callback({
+                recordsTotal: resp.recordsTotal,
+                recordsFiltered: resp.recordsFiltered,
+                data: mappedData,
+              });
+            });
+        }, DEFAULT_DELAY_TABLE);
       },
       columns: [
-        { data: 'kode_gudang', title: 'Kode Gudang' },
-        { data: 'nomor_pesanan', title: 'Nomor Pesanan' },
-        { data: 'kode_barang', title: 'Kode Barang' },
-        { data: 'satuan_kecil', title: 'Satuan' },
+        { data: 'kodeBarang', title: 'Kode Barang' },
+        { data: 'namaBarang', title: 'Nama Barang' },
+        { data: 'totalQtyPesan', title: 'Total Qty Pesan' },
+        { data: 'qtyBKirim', title: 'Qty Kirim Besar' },
+        { data: 'qtyKKirim', title: 'Qty Kirim Kecil' },
+        { data: 'konversi', title: 'Konversi' },
+        { data: 'totalQtyKirim', title: 'Total Qty Kirim' },
+        {
+          data: 'keterangan',
+          title: 'Keterangan',
+          render: (data) => {
+            if (data.toUpperCase() == STATUS_SAME_CONVERSION) {
+              return `
+                <span class="text-center text-success">${data}</span>
+              `;
+            } else {
+              return `
+                <span class="text-center text-danger">${data}</span>
+              `;
+            }
+          },
+        },
       ],
       order: [[1, 'asc']],
+      rowCallback: (row: Node, data: any[] | Object, index: number) => {
+        $('.action-view', row).on('click', () =>
+          this.actionBtnClick(ACTION_VIEW, data)
+        );
+        return row;
+      },
     };
+    this.dtColumns = this.dtOptions.columns;
   }
+  reloadTable() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  actionBtnClick(action: string, data: any = null): void {}
+
+  dtpageChange(event: any): void {}
 
   ngAfterViewInit(): void {
     this.dtTrigger.next(null);
   }
 
+  rerenderDatatable(): void {
+    this.dtOptions?.columns?.forEach((column: any, index) => {
+      if (this.dtColumns[index]?.title) {
+        column.title = this.translation.instant(this.dtColumns[index].title);
+      }
+    });
+  }
+
   ngOnDestroy(): void {
     this.dtTrigger.unsubscribe();
+    $.fn['dataTable'].ext.search.pop();
   }
 
-  search(): void {
-    this.datatableElement?.dtInstance.then((dtInstance: DataTables.Api) => {
-      dtInstance.ajax.reload();
-    });
+  onbackPressed(): void {
+    this.router.navigate(['/transaction/delivery-item/dobalik']);
   }
-
-  onPrint(): void {
-
-    console.log('Print button clicked');
-  
-  }
-
-  onButtonActionPressed(field:any): void{
-
-  }
-
-  dtPageChange(event: any): void {
-    console.log('Page changed', event);
-  }
-  
-  getDetailDoBalik(callback: any): void {
-    this.loading = true;
-
-    const params = {
-      kodeGudang: this.selectedOrder.kodeGudang,
-      nomorPesanan: this.selectedOrder.nomorPesanan,
-      tipeTransaksi: this.selectedOrder.tipeTransaksi,
-      noSuratJalan: this.selectedOrder.noSuratJalan,
-    };
-
-    console.log("Mengirim data ke backend:", params);
-
-    this.appService.getDetailDoBlik(params).subscribe(
-      (response: any) => {
-        this.detailDoBalikData = response.map((item: any) => ({
-          kode_gudang: item.KODE_GUDANG,
-          nomor_pesanan: item.NOMOR_PESANAN,
-          kode_barang: item.KODE_BARANG,
-          satuan_kecil: item.SATUAN_KECIL,
-        }));
-
-        callback({
-          recordsTotal: response.length,
-          recordsFiltered: response.length,
-          data: this.detailDoBalikData,
-        });
-
-        this.loading = false;
-      },
-      (error) => {
-        console.error('Error fetching data:', error);
-        this.loading = false;
-      }
-    );
-  }
-
-  onBackPressed(): void {
-    this.router.navigate(['/delivery-item/dobalik']);
-  }
-
-  onDelete(): void {
-    Swal.fire({
-      title: 'Konfirmasi Pembatalan',
-      text: 'Apakah Anda yakin ingin membatalkan pesanan ini?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Ya, Batalkan',
-      cancelButtonText: 'Tidak',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.updatingStatus = true;
-        const params = {
-          status: '4',
-          user: this.g.getUserCode(),
-          nomorPesanan: this.selectedOrder.nomorPesanan,
-        };
-
-        this.dataService.postData('/api/delivery-order/proses-do-balik', params).subscribe(
-          (response: any) => {
-            if (response.success) {
-              this.toastr.success('Pesanan berhasil dibatalkan');
-              this.router.navigate(['/order/receiving-order']);
-            } else {
-              this.toastr.error(response.message);
-            }
-            this.updatingStatus = false;
-          },
-          (error) => {
-            this.toastr.error('Gagal membatalkan pesanan');
-            this.updatingStatus = false;
-          }
-        );
-      }
-    });
-  }
-
-    formatStrDate(date: any) {
-      return moment(date, "YYYY-MM-DD").format("DD-MM-YYYY");
-    }
 }
--- src/app/views/transaction/dobalik/dobalik.component.ts
+++ src/app/views/transaction/dobalik/dobalik.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { DataTableDirective } from 'angular-datatables';
 import { Subject } from 'rxjs';
 import { DataService } from '../../../service/data.service';
@@ -8,8 +14,12 @@ import { AppConfig } from '../../../config/app.config';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { ToastrService } from 'ngx-toastr';
 import * as moment from 'moment';
-import { ACTION_VIEW, LS_INV_SELECTED_DELIVERY_ORDER } from '../../../../constants';
+import {
+  ACTION_VIEW,
+  LS_INV_SELECTED_DELIVERY_ORDER,
+} from '../../../../constants';
 import { AppService } from '../../../service/app.service';
+import { Page } from '../../../model/page';
 
 @Component({
   selector: 'app-dobalik',
@@ -17,19 +27,27 @@ import { AppService } from '../../../service/app.service';
   styleUrls: ['./dobalik.component.scss'],
 })
 export class DobalikComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChild(DataTableDirective, { static: false }) datatableElement: DataTableDirective | undefined;
-  
+  @ViewChild(DataTableDirective, { static: false }) datatableElement:
+    | DataTableDirective
+    | undefined;
+
   public dpConfig: Partial<BsDatepickerConfig> = new BsDatepickerConfig();
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
-  
+  page = new Page();
+
   startDate: Date = new Date();
   endDate: Date = new Date();
   minDate: Date = new Date();
   maxDate: Date = new Date();
-  
+
   loading: boolean = false;
+  showFilterSection: boolean = false;
   reportProposeData: any[] = [];
+  totalLength: number = 0;
+
+  dateRangeFilter: any = [new Date(), new Date()];
+  dataUser: any;
 
   constructor(
     private dataService: DataService,
@@ -44,21 +62,24 @@ export class DobalikComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnInit(): void {
     this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 10,
+      paging: true,
+      pageLength: 5,
+      lengthMenu: [5],
       processing: true,
-      serverSide: true,
-      ajax: (dataTablesParameters: any, callback) => {
+      ajax: 
+      (dataTablesParameters: any, callback) => {
         this.getProsesDoBalik(callback);
+        this.page.start = dataTablesParameters.start;
+        this.page.length = dataTablesParameters.length;
       },
       scrollX: true,
       autoWidth: true,
       columns: [
         { data: 'KODE_GUDANG', title: 'Kode Gudang' },
         { data: 'STATUS_POSTING', title: 'Status Posting' },
-        { data: 'TGL_TRANSAKSI', title: 'Tanggal Transaksi' },
+        { data: 'TGL_TRANSAKSI', title: 'Tanggal Transaksi', render:(data) => this.g.transformDate(data)},
         { data: 'TIPE_TRANSAKSI', title: 'Tipe Transaksi' },
-        { data: 'TGL_PESANAN', title: 'Tanggal Pesanan' },
+        { data: 'TGL_PESANAN', title: 'Tanggal Pesanan' , render:(data) => this.g.transformDate(data)},
         { data: 'NOMOR_PESANAN', title: 'Nomor Pesanan' },
         { data: 'NO_SURAT_JALAN', title: 'No Surat Jalan' },
         { data: 'KODE_TUJUAN', title: 'Kode Tujuan' },
@@ -67,7 +88,12 @@ export class DobalikComponent implements OnInit, AfterViewInit, OnDestroy {
           title: 'Opsi',
           className: 'text-center',
           render: () => {
-            return `<div class="d-flex px-2 gap-2"><button style="width: 100px" class="btn btn-sm action-posting btn-outline-success btn-60 pe-1">Posting</button> <button style="width: 100px" class="btn btn-sm action-view btn-outline-info btn-60">Lihat</button></div>  `;
+            return `
+              <div class="d-flex px-2 gap-2">
+                <button style="width: 100px" class="btn btn-sm action-posting btn-outline-success btn-60 pe-1">Posting</button>
+                <button style="width: 100px" class="btn btn-sm action-view btn-outline-info btn-60">Lihat</button>
+              </div>
+             `;
           },
         },
       ],
@@ -84,35 +110,39 @@ export class DobalikComponent implements OnInit, AfterViewInit, OnDestroy {
     };
   }
 
-  
-    actionBtnClick(action: string, data: any): void {
-      if (action === ACTION_VIEW) {
-        this.g.saveLocalstorage(
-          LS_INV_SELECTED_DELIVERY_ORDER,
-          JSON.stringify(data)
-        );
-        this.router.navigate(['/transaction/delivery-item/dobalik/detail-report-do-balik']); this
-      } if (action === 'POSTING') {
-        this.g.saveLocalstorage(
-          LS_INV_SELECTED_DELIVERY_ORDER,
-          JSON.stringify(data)
-        );
-        const param = {
-          kodeGudang: data.KODE_GUDANG, 
-          noSuratJalan: data.NO_SURAT_JALAN,
-          userPosted : JSON.parse(localStorage.getItem('inv_currentUser') || '').namaUser, 
-        }
-        this.appService.updateDeliveryOrderPostingStatus(param).subscribe({
-          next: (response) => {
-            this.toastr.success('Berhasil Posting DO Balik');
-            this.search();
-          },
-          error: (error) => {
-            this.toastr.error('Gagal Posting DO Balik');
-          },
-        })
-      }
+  actionBtnClick(action: string, data: any): void {
+    if (action === ACTION_VIEW) {
+      this.g.saveLocalstorage(
+        LS_INV_SELECTED_DELIVERY_ORDER,
+        JSON.stringify(data)
+      );
+      this.router.navigate([
+        '/transaction/delivery-item/dobalik/detail-report-do-balik',
+      ]);
+      this;
     }
+    if (action === 'POSTING') {
+      this.g.saveLocalstorage(
+        LS_INV_SELECTED_DELIVERY_ORDER,
+        JSON.stringify(data)
+      );
+      const param = {
+        kodeGudang: data.KODE_GUDANG,
+        noSuratJalan: data.NO_SURAT_JALAN,
+        userPosted: JSON.parse(localStorage.getItem('inv_currentUser') || '')
+          .namaUser,
+      };
+      this.appService.updateDeliveryOrderPostingStatus(param).subscribe({
+        next: (response) => {
+          this.toastr.success('Berhasil Posting DO Balik');
+          this.search();
+        },
+        error: (error) => {
+          this.toastr.error('Gagal Posting DO Balik');
+        },
+      });
+    }
+  }
 
   ngAfterViewInit(): void {
     this.dtTrigger.next(null);
@@ -122,9 +152,7 @@ export class DobalikComponent implements OnInit, AfterViewInit, OnDestroy {
     this.dtTrigger.unsubscribe();
   }
 
-  dtPageChange(event: any): void {
-    console.log('Page changed:', event);
-  }
+  dtPageChange(event: any): void {}
 
   search(): void {
     this.datatableElement?.dtInstance.then((dtInstance: DataTables.Api) => {
@@ -134,7 +162,7 @@ export class DobalikComponent implements OnInit, AfterViewInit, OnDestroy {
 
   getProsesDoBalik(callback: any): void {
     this.loading = true;
-    
+
     // Format tanggal menjadi 'dd MM yyyy' sebelum dikirim ke backend
     const formattedStartDate = moment(this.startDate).format('DD MMM yyyy');
     const formattedEndDate = moment(this.endDate).format('DD MMM yyyy');
@@ -142,17 +170,29 @@ export class DobalikComponent implements OnInit, AfterViewInit, OnDestroy {
     const params = {
       kodeGudang: this.g.getUserLocationCode(),
       statusPosting: 'I',
-      fromDate: formattedStartDate,  // Kirim dengan format 'dd MM yyyy'
-      toDate: formattedEndDate, // Jika perlu, bisa dikirim juga
+      fromDate: moment(
+        this.dateRangeFilter[0],
+        'ddd MMM DD YYYY HH:mm:ss [GMT]Z'
+      ).format('DD-MM-YYYY'), // Kirim dengan format 'dd MM yyyy'
+      toDate: moment(
+        this.dateRangeFilter[1],
+        'ddd MMM DD YYYY HH:mm:ss [GMT]Z'
+      ).format('DD-MM-YYYY'), // Jika perlu, bisa dikirim juga
     };
 
-    console.log("Mengirim data ke backend:", params);
+    console.log('Mengirim data ke backend:', params);
 
     this.dataService
-      .postData(this.config.BASE_URL + '/delivery-order/proses-do-balik', params)
+      .postData(
+        this.config.BASE_URL + '/delivery-order/proses-do-balik',
+        params
+      )
       .subscribe(
         (response: any) => {
+          let index = 0;
+          dtIndex: this.page.start + index + 1;
           this.reportProposeData = response.item;
+          this.totalLength = response.recordsTotal;
           callback({
             recordsTotal: response.recordsTotal,
             recordsFiltered: response.recordsFiltered,
@@ -165,5 +205,20 @@ export class DobalikComponent implements OnInit, AfterViewInit, OnDestroy {
           this.loading = false;
         }
       );
+  }
+
+  toggleFilter() {
+    this.showFilterSection = !this.showFilterSection;
+  }
+
+  onAddPressed() {
+    this.router.navigate(['/transaction/delivery-item/proses-do-balik']);
+  }
+
+  
+  onFilterPressed() {
+    this.datatableElement?.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.ajax.reload();
+    });
   }
 }
