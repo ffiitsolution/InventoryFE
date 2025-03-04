875e47c290989e6bedc64f028adfaee1604c5448
Muhamad Joko M <40954176+hijoem@users.noreply.github.com>
2025-03-04 17:04:46 +0700

    on progress reports

--- src/app/views/sync-data/sync-data.module.ts
+++ src/app/views/sync-data/sync-data.module.ts
@@ -5,42 +5,6 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MasterRoutingModule } from './master-routing.module';
-import { MasterUserComponent } from './master-user/master-user.component';
-import { MasterUserEditComponent } from './master-user/edit/edit.component';
-import { MasterUserDetailComponent } from './master-user/detail/detail.component';
-import {  MasterUserAddComponent } from './master-user/add/add.component';
-import { 
-  MasterBranchComponent, 
-  MasterBranchDetailComponent ,
-  MasterBranchAddComponent,
-  MasterBranchEditComponent
-} from './master-branch';
-import { MasterDepartmentComponent } from './master-department/master-department.component';
-import {
-  MasterProductAddComponent,
-  MasterProductComponent,
-  MasterProductDetailComponent,
-  MasterProductEditComponent,
-} from './master-product';
-import {
-  MasterSupplierAddComponent,
-  MasterSupplierComponent,
-  MasterSupplierDetailComponent,
-  MasterSupplierEditComponent,
-} from './master-supplier';
-import {
-  MasterLocationAddComponent,
-  MasterLocationComponent,
-  MasterLocationDetailComponent,
-  MasterLocationEditComponent,
-} from './master-location';
-import {
-  TableAreaAddComponent,
-  TableAreaComponent,
-  TableAreaDetailComponent,
-  TableAreaEditComponent,
-} from './table-area';
 import {
   ButtonDirective,
   ButtonGroupModule,
@@ -48,6 +12,7 @@ import {
   CollapseDirective,
   FormModule,
   GridModule,
+  ListGroupModule,
   ModalModule,
   TextColorDirective,
   UtilitiesModule,
@@ -57,40 +22,9 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { DataTablesModule } from 'angular-datatables';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MasterCompanyComponent } from './master-company/master-company.component';
-import {
-  TableRegionalComponent,
-  TableRegionalDetailComponent,
-  TableRegionalAddComponent,
-  TableRegionalEditComponent,
-} from './table-regional';
-import {
-  TableRscComponent,
-  TableRscAddComponent,
-  TableRscEditComponent,
-  TableRscDetailComponent,
-} from './table-rsc';
-import {
-  TableUomAddComponent,
-  TableUomComponent,
-  TableUomDetailComponent,
-  TableUomEditComponent,
-} from './table-uom';
-
-import {
-  TableCityAreaAddComponent,
-  TableCityAreaComponent,
-  TableCityAreaDetailComponent,
-  TableCityAreaEditComponent,
-} from './table-city-area';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import {
-  TableSetNumberComponent,
-  TableSetNumberAddComponent,
-  TableSetNumberEditComponent,
-  TableSetNumberDetailComponent,
-} from './table-set-number';
 import { SelectDropDownModule } from 'ngx-select-dropdown';
+import { ReportRoutingModule } from './report-routing.module';
+import { AllReportComponent } from './all-report/all-report.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -98,8 +32,8 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 @NgModule({
   imports: [
+    ReportRoutingModule,
     CommonModule,
-    MasterRoutingModule,
     CardModule,
     GridModule,
     HttpClientModule,
@@ -113,67 +47,13 @@ export function HttpLoaderFactory(http: HttpClient) {
     UtilitiesModule,
     ButtonGroupModule,
     ReactiveFormsModule,
-    BsDatepickerModule.forRoot(),
     ModalModule,
     SelectDropDownModule,
+    ListGroupModule,
   ],
   declarations: [
-    MasterUserComponent,
-    MasterUserDetailComponent,
-    MasterUserAddComponent,
-    MasterUserEditComponent,
-    MasterCompanyComponent,
-    MasterBranchComponent,
-    MasterBranchDetailComponent,
-    MasterBranchAddComponent,
-    MasterBranchEditComponent,
-    MasterDepartmentComponent,
-
-    MasterProductComponent,
-    MasterProductAddComponent,
-    MasterProductDetailComponent,
-    MasterProductEditComponent,
-
-    MasterSupplierComponent,
-    MasterSupplierAddComponent,
-    MasterSupplierDetailComponent,
-    MasterSupplierEditComponent,
-
-    MasterLocationComponent,
-    MasterLocationAddComponent,
-    MasterLocationDetailComponent,
-    MasterLocationEditComponent,
-
-    TableAreaComponent,
-    TableAreaAddComponent,
-    TableAreaDetailComponent,
-    TableAreaEditComponent,
-
-    TableRegionalComponent,
-    TableRegionalDetailComponent,
-    TableRegionalAddComponent,
-    TableRegionalEditComponent,
-
-    TableRscComponent,
-    TableRscAddComponent,
-    TableRscDetailComponent,
-    TableRscEditComponent,
-
-    TableUomComponent,
-    TableUomAddComponent,
-    TableUomDetailComponent,
-    TableUomEditComponent,
-
-    TableCityAreaComponent,
-    TableCityAreaAddComponent,
-    TableCityAreaDetailComponent,
-    TableCityAreaEditComponent,
-
-    TableSetNumberComponent,
-    TableSetNumberAddComponent,
-    TableSetNumberEditComponent,
-    TableSetNumberDetailComponent,
+    AllReportComponent,
   ],
   schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
 })
-export class MasterModule {}
+export class ReportModule {}
