19839a1a04cac9cbdbc443e0d022f6b311e9c624
mrashadyusuf <mmrashadyusuf@gmrail.com>
2025-04-28 13:19:16 +0700

    add pagination add search on receiving order - order manual page

--- src/app/views/order/receiving-order/order-manual/add-data-detail/add-data-detail.component.ts
+++ src/app/views/order/receiving-order/order-manual/add-data-detail/add-data-detail.component.ts
@@ -78,6 +78,13 @@ export class AddDataDetailOrderManualComponent
   barangTemp: any[] = [];
   isShowModalCancel: boolean = false;
 
+  listCurrentPage: number = 1;
+  itemsPerPage: number = 5;
+
+  searchListViewOrder: string = '';
+
+
+
   @ViewChild('formModal') formModal: any;
 
 
@@ -622,6 +629,29 @@ export class AddDataDetailOrderManualComponent
       this.router.navigate(['/order/receiving-order/order-manual']);
     });
   }
+  onFilterTextChange(newValue: string) {
+    if (newValue.length >= 3) {
+      this.totalLength = 1;
+    } else {
+      this.totalLength = this.listOrderData.length;
+    }
+    this.listCurrentPage = this.listCurrentPage;
+  }
+
+    get filteredList() {
+    if (!this.searchListViewOrder) {
+      return this.listOrderData;
+    }
+    const searchText = this.searchListViewOrder.toLowerCase();
+    return this.listOrderData.filter(item =>
+      JSON.stringify(item).toLowerCase().includes(searchText)
+    );
+  }
+
+  getPaginationIndex(i: number): number {
+    return (this.listCurrentPage - 1) * this.itemsPerPage + i;
+  }
+  
 
 
 }
